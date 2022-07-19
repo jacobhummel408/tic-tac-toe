87073b92b5b9386bfe267f6e1473d89898e82b7f
jacobhummel408 <jacobhummel408@gmail.com>
2022-07-19 13:01:58 -0400

    chore: remove tailwindcss config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./assets/styles/index.css";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./App";
 
